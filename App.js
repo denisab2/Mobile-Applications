6ee782456fa0c6a733cdfa93d4276b7b84311271
denisab2 <denisabica@yahoo.com>
2017-11-26 16:58:00 +0200

    Lab5 react.

--- App.js
+++ App.js
@@ -14,88 +14,11 @@ import {
   TextInput
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import styles from './styles';
+import HomeScreen from './components/HomeScreen';
+import UpdateScreen from './components/UpdateScreen';
+import CreateScreen from './components/CreateScreen';
 
-let products=[
-  {name:'egg',price:'2', quantity:'20'},
-  {name:'bread',price:'5', quantity:'2'},
-  {name:'milk',price:'6', quantity:'5'}
-]
-
-
-  
-  /**<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Home Screen</Text>
-    <Button
-      onPress={() => navigation.navigate('Details')}
-      title="Go to details"
-    />
-  </View>*/
-
-class DetailsScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: ''
-    };
-  }
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <TextInput value={this.state.name || this.props.navigation.state.params.item.name} onChangeText={(text) => this.setState({name: text})}></TextInput>
-        <Button
-          onPress={() => this.props.navigation.state.params.fct(this.props.navigation.state.params.item, this.state.name)}
-          title="Save"
-        />
-      </View>
-    );
-  }
-}
-
-class HomeScreen extends Component<{}> {
-
-  constructor(props) 
-  {
-      super(props);
-      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      this.state = {
-        dataSource: ds.cloneWithRows(products), 
-      };
-    }
-
-      functie(id, name) {
-            var temp = [];
-            temp = products.slice();
-          temp[0].name = name;
-          this.state = {
-            dataSource: this.state.dataSource.cloneWithRows(temp), 
-          };
-      }
-
-     renderRow(rowData)
-      {
-       return (
-              <View style={styles.welcome} >
-              <TouchableOpacity  onPress={() => this.props.navigation.navigate('Details', {item: rowData, fct: this.functie.bind(this)})}>
-                <Text style={styles.instructions}> {rowData.name}   Price: {rowData.price}  Quantity: {rowData.quantity}
-                </Text>
-
-              </TouchableOpacity>
-              </View>
-            );
-      }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <ListView 
-        dataSource={this.state.dataSource}
-        renderRow={this.renderRow.bind(this)}
-        />
-      </View>
-      );
-  }
-}
 
 const RootNavigator = StackNavigator({
   Home: {
@@ -104,33 +27,18 @@ const RootNavigator = StackNavigator({
       headerTitle: 'Home',
     },
   },
-  Details: {
-    screen: DetailsScreen,
+  Update: {
+    screen: UpdateScreen,
     navigationOptions: {
-      headerTitle: 'Details',
+      headerTitle: 'Update',
+    },
+  },
+  Create: {
+    screen: CreateScreen,
+    navigationOptions: {
+      headerTitle: 'Create',
     },
   },
 });
 
 export default RootNavigator;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-    fontSize: 20,
-  },
-});
-
---
+++ components/HomeScreen.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  Button,
+  View,ListView, TouchableOpacity,
+  TextInput
+} from 'react-native';
+
+import storageService from '../services/StorageService';
+import { styles } from '../styles';
+
+export default class HomeScreen extends Component {
+
+  constructor(props) 
+  {
+      super(props);
+      this.state = {
+        products: [],
+        dataSource: null,
+      };
+    }
+
+   async componentDidMount() {
+      this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      const products = await storageService.getProducts();
+      this.setState({
+        products: products,
+        dataSource: this.ds.cloneWithRows(products),
+      });
+    }
+
+    async update(newProducts){
+      this.setState({
+        products: newProducts,
+        dataSource: this.ds.cloneWithRows(newProducts)
+      });
+      await storageService.setProducts(newProducts);
+    }
+
+    delete(id) {
+
+    }
+
+    renderRow(rowData)
+    {
+       return (
+        <View style={styles.welcome} >
+          <TouchableOpacity  onPress={() => this.props.navigation.navigate('Update', {item: rowData, update: this.update.bind(this), products: this.state.products})}>
+            <Text style={styles.instructions}> {rowData.name} Price: {rowData.price}  Quantity: {rowData.quantity}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+
+  render() {
+    if (!this.state.dataSource) {
+      return (
+        <View style={styles.container}>
+        </View>
+      );
+    }
+    return (
+      <View style={styles.container}>
+        <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow.bind(this)}
+        />
+        <Button
+          onPress={() => this.props.navigation.navigate('Create', {update: this.update.bind(this), products: this.state.products})}
+          title="Add new"
+        />
+      </View>
+
+       
+      );
+  }
+}
